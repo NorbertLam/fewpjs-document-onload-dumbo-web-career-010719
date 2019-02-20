9ff571d6a14b6c5609e0053af2d91a289ac8a2a6
NorbertLam <noobertlm@gmail.com>
2019-02-19 22:30:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+function domContentLoaded() {
+  document.addEventListener("DOMContentLoaded", domLoaded, false);
+ 
+  function domLoaded(event) {
+    updateDOM();
+  }
+}
+
+function updateDOM() {
+  const text = document.querySelector('p#text');
+
+  text.textContent = "This is really cool!";
+}
